9874af214f887635b4ff092f63618f7f5406d38f
Cristian Schuszter <cristian.schuszter@cern.ch>
2020-11-28 21:47:49 +0100

    Query for monthly footprint

--- src/controllers/footprint.controller.ts
+++ src/controllers/footprint.controller.ts
@@ -1,5 +1,7 @@
 import { transport } from "carbon-footprint";
 import { Request, Response } from "express";
+import moment from "moment";
+import { Op } from "sequelize";
 import { Footprint } from "../model/footprint.model";
 
 import { User } from "../model/user.model";
@@ -19,14 +21,18 @@ const transportAndDistanceToFootprint = (
     case "jogging":
     case "walking":
       value = 0;
+      break;
     case "biking":
       value = transport.motorbike;
+      break;
     case "driving":
       value = transport.car;
+      break;
     default:
       value = transport.car;
+      break;
   }
-  return value * totalDistance;
+  return value * (totalDistance * 1000);
 };
 
 export class FootprintController {
@@ -51,5 +57,24 @@ export class FootprintController {
     })
       .then((data: Footprint) => res.status(201).json(data))
       .catch((err: Error) => res.status(500).json(err));
+  }
+
+  public async getMonthlyFootprint(req: Request, res: Response) {
+    const sessionUser = res.locals.user as User;
+    const footprint = sessionUser.computeDailyCarbonFootprint();
+    const dateFrom = moment(Date.now()).startOf("month");
+    const footprints = await Footprint.findAll({
+      where: {
+        createdAt: {
+          [Op.gt]: dateFrom.toDate(),
+          [Op.lte]: Date.now(),
+        },
+        userId: sessionUser.id,
+      },
+    });
+
+    const totalMonthly = footprints.reduce((a, b) => a + b.value, 0);
+    const totalAggregatedDaily = footprint * moment().diff(dateFrom, "days");
+    res.status(200).json({ total: totalMonthly + totalAggregatedDaily });
   }
 }
