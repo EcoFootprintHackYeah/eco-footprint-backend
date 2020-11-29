a188d85824faf07b56717fae01a0bd7c575ecc1d
Cristian Schuszter <cristian.schuszter@cern.ch>
2020-11-29 07:11:51 +0100

    Fix race cond

--- src/app.ts
+++ src/app.ts
@@ -26,11 +26,11 @@ class Server {
     this.routePrv.routes(this.app);
   }
 
-  private config(): void {
+  private async config(): Promise<void> {
     this.app.use(bodyParser.json());
     this.app.use(bodyParser.urlencoded({ extended: false }));
     this.app.use(cors());
-    createDb(false);
+    await createDb(true);
   }
 }
 
--- src/model/initDb.ts
+++ src/model/initDb.ts
@@ -2,13 +2,12 @@ import { Footprint } from "./footprint.model";
 import { HistoricalData } from "./historical.model";
 import { User } from "./user.model";
 
-export const createDb = (force: boolean) => {
+export const createDb = async (force: boolean) => {
   // syncs
-  User.sync({ force: force }).then(() => console.log("User table created"));
-  Footprint.sync({ force: force }).then(() =>
-    console.log("footprints table created")
-  );
-  HistoricalData.sync({ force: force }).then(() =>
-    console.log("created historical data")
-  );
+  await User.sync({ force: force });
+  console.log("Users created");
+  await Footprint.sync({ force: force });
+  console.log("footprints table created");
+  await HistoricalData.sync({ force: force });
+  console.log("history table created");
 };
